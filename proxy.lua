6468ada32855b993682e9cd369ea1e6c7de28d54
ivan386 <ivan386@yandex.ru>
2016-02-04 02:13:58 +0300

    Anti filter

--- proxy.lua
+++ proxy.lua
@@ -1,104 +1,8 @@
 require "socket"
 
-local dns_cache = {
-	
-}
-
-function to_ip(host)
-	if not dns_cache[host] then
-		return socket.dns.toip(host)
-	end
-	return dns_cache[host] 
-end
-
-function get_data(data, err, part)
-	--if err then print(err) end
-	return (data or part), err
-end
-
-function get_headers(_in_)
-	local headers, end_index = ""
-	repeat
-		data, err = get_data(_in_:receive("*a"))
-		if (err and (err ~= "timeout")) then
-			_in_:close()
-			return
-		end
-		if data then
-			headers = headers..data
-			end_index = headers:find("\r\n\r\n", 1, true)
-			if end_index then
-				break
-			end
-		end
-		coroutine.yield()
-	until false
-	return headers, end_index
-end
-
-function send_data(out, data)
-	local index = 0
-	repeat
-		index = index +1
-		index, err = get_data(out:send(data, index))
-	until index == #data or (err and (err ~= "timeout"))
-end
-
-function cycle_data(p, tp)
-	local _in_, out = p, tp
-	repeat
-		_in_:settimeout(0)
-		local data, err = get_data(_in_:receive("*a"))
-		if data and (#data > 0) then
-			out:settimeout(1)
-			send_data(out, data) 
-		end
-		_in_, out = out, _in_
-		coroutine.yield()
-	until err and (err ~= "timeout")
-end
-local hard_replace = true
-
-function new_client(client)
-	client:settimeout(0)
-	local data, err 
-	local headers, end_index = get_headers(client)
-	local hots, path
-
-	if headers then
-		local path_st, path_en
-		path_st, path_en, method, path = headers:find("([PpGg][OoEe][SsTt][Tt]?)%s([^%s]+)")
-		_, _, host = headers:find("\r\n[Hh][Oo][Ss][Tt]:%s([^%s]+)%s")
-		local _, end_idx = path:find(host, 1, true)
-		if end_idx then
-			start_idx, end_idx = path:find("/", end_idx)
-			local new_path = path:sub(end_idx)
-			if hard_replace then
-				headers = string.format("%s %s HTTP/1.0\r\nHost: %s", method, new_path, host,headers:sub(end_index))
-			else
-				headers = string.format("%s %s%s", method, new_path, headers:sub(path_en+1))
-			end
-			
-		end
-		headers = headers:gsub("\r\nHost: ", "\r\nhost: ") --antifilter
-	else
-		return
-	end
-	
-	if path and host then
-		print(headers)
-		local server = socket.connect(to_ip(host), 80)
-		if server then
-			server:send(headers)
-			cycle_data(server, client)
-			server:close()
-		end
-	end
-	client:close()
-end
-
 local cors = {}
 function main()
+	print "main"
 	local proxy_bind = socket.tcp()
 	proxy_bind:setoption("reuseaddr", true)
 	proxy_bind:bind("localhost", 80)
@@ -106,26 +10,97 @@ function main()
 	proxy_bind:settimeout(0)
 	while true do
 		local client = proxy_bind:accept()
-		if client then
+		if (client) then
 			local new_cor = coroutine.create(new_client)
 			local ok, err = coroutine.resume(new_cor, client)
-			if ok then
+			if (ok) then
 				table.insert(cors, new_cor)
 			end
 		end
 		local new_list = {}
 		for id, cor in ipairs(cors) do
 			local ok, err = coroutine.resume(cor)
-			if ok then
+			if (ok) then
 				table.insert(new_list, cor)
 			end
 		end
 		cors = new_list
+		socket.sleep(0.001)
 	end
 end
 
-local mainco = coroutine.create(main)
-local ok, err = coroutine.resume(mainco)
-if not ok then
-	print(debug.traceback(mainco, err))
+function new_client(client)
+	print "new_client"
+	client:settimeout(0)
+	local headers, err = get_data(client:receive("*a"))
+	local host, port = get_host_port(headers)
+	local server = socket.connect(host, port)
+	if (server) then
+		if (send_connect(server, string.format("%s:%s", socket.dns.toip(host), port))) then
+			server:send(headers)
+			cycle_data(server, client, host)
+		end
+	end
+	server:close()
+	client:close()
+	print "client, server close"
 end
+
+function get_data(data, err, part, marker)
+	data = (data or part)
+	return data, err
+end
+
+function get_host_port(headers)
+	local _, _, host, port = headers:find(" HTTP/1.1\13\10Host: ([a-z0-9%.%-]+):?([0-9]*)\13\10")
+	if (#port > 0) then
+		port = tonumber(port)
+	else
+		port = 80
+	end
+	print(host, port)
+	return host, port
+end
+
+
+connect = "CONNECT %s HTTP/1.1\13\10Host: %s\13\10\13\10"
+function send_connect(server, address)
+	server:send(string.format(connect, address, address))
+	server:settimeout(0.1)
+	local headers, err = get_data(server:receive("*a"))
+	return headers:find("^HTTP/1.[01] 200")
+end
+
+function cycle_data(server, client, host, port)
+	local _in_, out = server, client
+	repeat
+		_in_:settimeout(0)
+		out:settimeout(1)
+		
+		local data, receive_err = get_data(_in_:receive("*a"))
+		
+		if data and (#data > 0) then
+			local index, send_err = send_data(out, data)
+		end
+		
+		coroutine.yield()
+		
+		_in_, out = out, _in_
+	until (receive_err and (receive_err ~= "timeout")) or (send_err and (send_err ~= "timeout"))
+end
+
+function send_data(out, data)
+	local index = 0
+	repeat
+		index = index +1
+		index, err = get_index(out:send(data, index))
+	until index >= #data or (err and (err ~= "timeout"))
+	
+	return index, err
+end
+
+function get_index(index, err, partial_index)
+	return (index or partial_index), err
+end
+
+main()
