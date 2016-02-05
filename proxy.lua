53b0c8b66ccc22f809d1f33285c52d5ecf0776a0
ivan386 <ivan386@yandex.ru>
2016-02-05 23:04:09 +0300

    Update

--- proxy.lua
+++ proxy.lua
@@ -2,14 +2,13 @@ require "socket"
 
 local cors = {}
 function main()
-	print "main"
 	local proxy_bind = socket.tcp()
-	proxy_bind:setoption("reuseaddr", true)
-	proxy_bind:bind("localhost", 80)
-	proxy_bind:listen(100)
-	proxy_bind:settimeout(0)
-	while true do
-		local client = proxy_bind:accept()
+	proxy_bind:setoption("reuseaddr", true) -- На всякий случай
+	proxy_bind:bind("localhost", 8080) -- Принимаем подключения только от локального клиента
+	proxy_bind:listen(100) -- 100 подключений в очереди
+	proxy_bind:settimeout(0) -- 0s Позволяет не задерживаться ожидая данных и забрать из буфера сразу всё.
+	repeat -- Главный цикл который принимает подключения
+		local client, accept_err = proxy_bind:accept()
 		if (client) then
 			local new_cor = coroutine.create(new_client)
 			local ok, err = coroutine.resume(new_cor, client)
@@ -25,25 +24,31 @@ function main()
 			end
 		end
 		cors = new_list
-		socket.sleep(0.001)
-	end
+		socket.sleep(0.001) -- Чтоб не сильно нагружать процессор
+	until accept_err and (accept_err ~= "timeout")
 end
 
+-- Читаем хедеры и соединяемся с конечным сайтом сквозь фильтрующий прокси
 function new_client(client)
-	print "new_client"
 	client:settimeout(0)
 	local headers, err = get_data(client:receive("*a"))
 	local host, port = get_host_port(headers)
 	local server = socket.connect(host, port)
+	print("NC", host, port)
 	if (server) then
-		if (send_connect(server, string.format("%s:%s", socket.dns.toip(host), port))) then
+		if (not send_connect(server, string.format("%s:%s", socket.dns.toip(host), port))) then
+			server:close()
+			print("DIRECT", host, port)
+			server = socket.connect(host, port)
+		end
+		if (server) then
 			server:send(headers)
 			cycle_data(server, client, host)
 		end
 	end
+	print("CLOSED", host, port)
 	server:close()
 	client:close()
-	print "client, server close"
 end
 
 function get_data(data, err, part, marker)
@@ -52,24 +57,37 @@ function get_data(data, err, part, marker)
 end
 
 function get_host_port(headers)
-	local _, _, host, port = headers:find(" HTTP/1.1\13\10Host: ([a-z0-9%.%-]+):?([0-9]*)\13\10")
-	if (#port > 0) then
-		port = tonumber(port)
-	else
-		port = 80
+	if headers and (#headers > 0) then
+		if not (headers:find(" HTTP/1.1\13\10Host: ", 1, true)) then
+			return
+		end
+		local _, _, host, port = headers:find(" HTTP/1.1\13\10Host: ([a-z0-9%.%-]+):?([0-9]*)\13\10")
+		if (#port > 0) then
+			port = tonumber(port)
+		else
+			port = 80
+		end
+		return host, port
 	end
-	print(host, port)
-	return host, port
 end
 
-
-connect = "CONNECT %s HTTP/1.1\13\10Host: %s\13\10\13\10"
+-- Используем метод CONNECT который превращает фильтрующий прокси в TCP трубу
+local connect = "CONNECT %s HTTP/1.1\13\10Host: %s\13\10\13\10"
 function send_connect(server, address)
 	server:send(string.format(connect, address, address))
 	server:settimeout(0.1)
 	local headers, err = get_data(server:receive("*a"))
+	while ((not headers) or (#headers <= 12)) and not (err and (err ~= "timeout")) do
+		coroutine.yield() -- Даём скрипту обработать другие подключения
+		local data, err = get_data(server:receive("*a"))
+		if (data and #data > 0) then
+			headers = (headers or "") .. data
+		end
+	end
 	return headers:find("^HTTP/1.[01] 200")
 end
+
+-- Пересылаем данные в обе стороны
 
 function cycle_data(server, client, host, port)
 	local _in_, out = server, client
@@ -78,14 +96,14 @@ function cycle_data(server, client, host, port)
 		out:settimeout(1)
 		
 		local data, receive_err = get_data(_in_:receive("*a"))
-		
+
 		if data and (#data > 0) then
+			data = data:gsub("\13\10Connection%: keep%-alive\13\10", "\13\10Connection: close\13\10")
 			local index, send_err = send_data(out, data)
 		end
 		
-		coroutine.yield()
+		coroutine.yield() -- Даём скрипту обработать другие подключения
 		
-		_in_, out = out, _in_
 	until (receive_err and (receive_err ~= "timeout")) or (send_err and (send_err ~= "timeout"))
 end
 
--- run.bat
+++ run.bat
@@ -1,2 +1,4 @@
+%~d0
+cd %~p0
 lua5.1 proxy.lua
 pause
