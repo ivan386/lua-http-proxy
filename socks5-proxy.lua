06e06997253ca5926643da62581307e385deb1d0
ivan386 <ivan386@yandex.ru>
2017-05-24 01:10:49 +0300

    Update

--- socks5-proxy.lua
+++ socks5-proxy.lua
@@ -1,6 +1,36 @@
 require "socket"
+require "serialize3"
 
 local cors = {}
+local cors_id = {}
+local last_id = 1
+
+print_old = print
+
+function get_coroutine_index(co)
+	if not co then co = coroutine.running() end
+	if co then
+		local index = cors_id[co]
+		if not index then
+			index = last_id
+			cors_id[co] = last_id
+			last_id = last_id + 1
+		end
+		return index
+	else
+		return 0
+	end
+end
+
+function print(...)
+	print_old(get_coroutine_index(), ...)
+end
+
+function temp_print(...)
+	io.stdout:write(get_coroutine_index(), "\t")
+	io.stdout:write(table.concat({...},"\t"))
+	io.stdout:write("\r")
+end
 
 
 function get_data(data, err, part, marker)
@@ -14,11 +44,11 @@ function connect_to(host, port)
 	local connected = server:connect(host, port)
 	if not connected then
 		repeat
+			coroutine.yield() -- Даём скрипту обработать другие подключения
 			local _, ready = socket.select({}, {server}, 0)
 			if #ready > 0 then
 				return server
 			end
-			coroutine.yield() -- Даём скрипту обработать другие подключения
 		until false
 	else
 		return server
@@ -29,14 +59,14 @@ function get_index(index, err, partial_index)
 	return (index or partial_index), err
 end
 
-function send_data(out, data)
-	local index = 0
+function send_data(out, data, index, stop)
+	if not index then index = 1 end
+	if not stop then stop = #data end
 	repeat
-		index = index +1
-		index, err = get_index(out:send(data, index))
-		local all_sended = index >= #data or (err and (err ~= "timeout"))
+		
+		index, err = get_index(out:send(data, index, stop))
+		local all_sended = index >= stop or (err and (err ~= "timeout"))
 		if not(all_sended) then
-			print ("index:", index)
 			coroutine.yield() -- Даём скрипту обработать другие подключения
 		end
 	until all_sended
@@ -44,88 +74,137 @@ function send_data(out, data)
 	return index, err
 end
 
+function split_send(socket, data, ...)
+	local positions = {...}
+	local from = 1
+	for i, to in ipairs( positions ) do
+		send_data(socket, data, from, to)
+		from = to + 1
+	end
+	send_data(socket, data, from)
+end
+
+function filtred(out, data)
+	local _, host_pos = data:find("Host:", 1, true)
+	if not host_pos then return false end
+	split_send(out, data, host_pos)
+	local line_end = data:find("[\n\r]", host_pos)
+	print("Host detected:", data:sub(host_pos+1, line_end-1))
+	return true
+end
+
 function cycle_data(server, client)
 	local _in_, out = server, client
 	local receive_err, send_err, data, index
 	repeat
 		_in_:settimeout(0)
-		out:settimeout(1)
+		out:settimeout(0.1)
 		
 		data, receive_err = get_data(_in_:receive("*a"))
 
 		if data and (#data > 0) then
-			index, send_err = send_data(out, data)
+			temp_print(#data)
+			if not filtred( out, data ) then
+				index, send_err = send_data(out, data)
+			end
 		end
 		coroutine.yield() -- Даём скрипту обработать другие подключения
 		_in_, out = out, _in_
 		
 	until (receive_err and (receive_err ~= "timeout")) or (send_err and (send_err ~= "timeout"))
-	print("cycle:", receive_err, send_err)
+	
+end
+
+function wait_for_data(_in_, min_count)
+	local buf, err = ""
+	local part
+	repeat
+		part, err = get_data(_in_:receive("*a"))
+		if part and #part > 0 then
+			buf = buf..part
+		end
+	until (#buf >= min_count or (err and (err ~= "timeout")))
+	
+	return buf, err
 end
 
 function new_client(client)
-	client:settimeout(0.3)
-    local header = get_data(client:receive("*a"))
-    if ( header:byte(1) ~= 5 ) then
-        print("wrong wersion: ", header:byte(1))
-        client:close()
-        return
-    end
-    local autority_count = header:byte(2)
-    if ( #header < autority_count + 2 ) then
-        print("partial data: ", autority_count + 2, #header)
-        client:close()
-        return
-    else
-        print("count:", autority_count)
-    end
-    
-    if ( header:find("\0", 1, true) ) then
-        print("no autority found")
-    end
-    client:send("\5\0")
-    header = get_data(client:receive("*a"))
-    
-    if ( header:sub(1,3) == "\5\1\0" ) then
-        
-    else
-        print("wrong responce: ", #header, header)
-        client:close()
-    end
-    
-    local address
-    local next_pos
-    if ( header:byte(4) == 1 ) then -- IPv4
-        address = string.format("%s.%s.%s.%s", header:byte(5,6,7,8))
-        print("ipv4:", address)
-        next_pos = 9
-    elseif ( header:byte(4) == 3 ) then -- [len] domain
-        address = header:sub(6, 6 + header:byte(5) - 1)
-        print("domain:", address)
-        next_pos = 6 + header:byte(5)
-    elseif ( header:byte(4) == 4 ) then
-        print("ipv6")
-        next_pos = 20
-    end
-    
-    local port = header:byte(next_pos) * 256 + header:byte(next_pos + 1)
-    print("port:", port)
-    
-    local server = connect_to(address, port)
-    if (server) then
-        client:send("\5\0\0\1\0\0\0\0\0\0")
-        print("conected to:", address, port)
-        cycle_data(client, server)
-        server:close()
-    else
-        client:send("\5\0\0\4\0\0\0\0\0\0")
-        print("not conected to:", address, port)
-    end
-    
-    client:close()
- end
- 
- function main()
+	print "new client"
+	client:settimeout(0)
+	local header, err = wait_for_data(client, 2)
+	
+	-- Версия socks протокола должна быть 5
+	if ( header:byte(1) ~= 5 ) then
+		print("wrong wersion: ", safe_string(header), safe_string(err))
+		client:close()
+		return
+	end
+	
+	-- Количество методов авторизации
+	local autority_count = header:byte(2)
+	if ( #header < autority_count + 2 ) then
+		print("partial data: ", autority_count + 2, #header)
+		client:close()
+		return
+	else
+		print("autority count:", autority_count)
+	end
+	
+	-- Есть ли в списке "Без авторизации"
+	if ( header:find("\0", 1, true) ) then
+		print("'no autority' found")
+	else
+		print("'no autority' not found")
+		client:close()
+		return
+	end
+	
+	client:send("\5\0")
+	header = wait_for_data(client, 8)
+	
+	-- Принимаем запросы только на TCP/IP соеденение
+	if ( header:sub(1,3) == "\5\1\0" ) then
+		print("tcp connection request")
+	else
+		print("wrong responce: ", #header, header)
+		client:close()
+		return
+	end
+	
+	local address
+	local next_pos
+	if ( header:byte(4) == 1 ) then -- IPv4
+		address = string.format("%s.%s.%s.%s", header:byte(5,6,7,8))
+		print("ipv4:", address)
+		next_pos = 9
+	elseif ( header:byte(4) == 3 ) then -- [len] domain
+		address = header:sub(6, 6 + header:byte(5) - 1)
+		print("domain:", address)
+		next_pos = 6 + header:byte(5)
+	elseif ( header:byte(4) == 4 ) then
+		print("ipv6")
+		next_pos = 20
+	end
+	
+	local port = header:byte(next_pos) * 256 + header:byte(next_pos + 1)
+	print("port:", port)
+	
+	local server = connect_to(address, port)
+	if (server) then
+		client:send("\5\0\0\1\0\0\0\0\0\0")
+		print("conected to:", address, port)
+		cycle_data(client, server)
+		server:close()
+	else
+		client:send("\5\0\0\4\0\0\0\0\0\0")
+		print("not conected to:", address, port)
+	end
+	
+	print("closed:", address, port)
+	client:close()
+end
+
+function main()
 	local proxy_bind = socket.tcp()
 	proxy_bind:setoption("reuseaddr", true) -- На всякий случай
 	proxy_bind:bind("localhost", 8080) -- Принимаем подключения только от локального клиента
@@ -151,5 +230,5 @@ function new_client(client)
 		socket.sleep(0.001) -- Чтоб не сильно нагружать процессор
 	until accept_err and (accept_err ~= "timeout")
 end
- 
- main()
+
+main()
